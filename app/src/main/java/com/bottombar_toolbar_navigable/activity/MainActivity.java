9279499406a34278bd8ef29f7c77afbb9dd3bdbf
ramesh <ramesh.kvana@gmail.com>
2017-01-04 19:41:35 +0530

    Searching functionality is added

--- app/src/main/java/com/bottombar_toolbar_navigable/activity/MainActivity.java
+++ app/src/main/java/com/bottombar_toolbar_navigable/activity/MainActivity.java
@@ -10,12 +10,17 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.bottombar_toolbar_navigable.R;
@@ -24,14 +29,23 @@ import com.bottombar_toolbar_navigable.fragment.FragmentGallery;
 import com.bottombar_toolbar_navigable.fragment.SendFragment;
 import com.bottombar_toolbar_navigable.fragment.SlideFragment;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static com.bottombar_toolbar_navigable.R.menu.main;
 
-public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener, TextWatcher {
     private static final String TAG = "MainActivity";
     private ImageView cameraIV, galleryIV, slideIV, sendIV;
     private TextView cameraTV, galleryTV, slideTV, sendTV;
     private LinearLayout cameraRL, galleryRL, slideRL, sendRL;
     private FrameLayout content_frame;
+    private View view;
+    String[] items;
+    ArrayList<String> listItems;
+    ArrayAdapter<String> adapter;
+    ListView list_view;
+    EditText et_search;
 
     public static void start(Context context) {
         Intent starter = new Intent(context, MainActivity.class);
@@ -54,6 +68,11 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        list_view = (ListView) findViewById(R.id.list_view);
+        et_search = (EditText) findViewById(R.id.et_search);
+        et_search.addTextChangedListener(this);
+        initList();
+
         content_frame = (FrameLayout) findViewById(R.id.content_frame);
         cameraIV = (ImageView) findViewById(R.id.cameraIV);
         galleryIV = (ImageView) findViewById(R.id.galleryIV);
@@ -74,6 +93,28 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
         sendRL = (LinearLayout) findViewById(R.id.sendRL);
         sendRL.setOnClickListener(this);
     }
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        if (charSequence.toString().equals("")) {
+            // reset listview
+            initList();
+        } else {
+            // perform search
+            searchItem(charSequence.toString());
+        }
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+
+    }
+
 
     @Override
     public void onClick(View view) {
@@ -181,5 +222,21 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
         galleryIV.setImageResource(R.drawable.ic_menu_gallery);
         slideIV.setImageResource(R.drawable.ic_menu_slideshow);
         sendIV.setImageResource(R.drawable.ic_menu_send);
+    }
+
+    public void initList() {
+        items = new String[]{"Canada", "China", "Japan", "USA", "Germany", "Australia"};
+        listItems = new ArrayList<>(Arrays.asList(items));
+        adapter = new ArrayAdapter<>(this, R.layout.row_item, R.id.tv_search, listItems);
+        list_view.setAdapter(adapter);
+    }
+
+    public void searchItem(String textToSearch) {
+        for (String item : items) {
+            if (!item.contains(textToSearch)) {
+                listItems.remove(item);
+            }
+        }
+        adapter.notifyDataSetChanged();
     }
 }
